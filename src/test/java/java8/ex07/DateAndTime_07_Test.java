c335ed07071344aa712965d3c3b78dbbf4199eb4
Jeremy CINELLI <jeremy.cinelli@gmail.com>
2020-07-15 15:08:58 +0200

    Exercices Date & Time

--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -22,17 +22,13 @@ public class DateAndTime_07_Test {
 
         // TODO Créer une date Java 1 (12/02/2017)
         Date date = new Date(117, 1, 12);
-        System.out.println(date);
 
         // TODO transformer la date en Instant
         Instant instant = date.toInstant();
         
-        System.out.println(instant);
-
         // TODO transformer la date en LocalDate
 
         LocalDate result = LocalDate.ofInstant(instant, ZoneId.systemDefault());
-        System.out.println(result);
         // TODO valoriser les différentes variables afin de rendre le test passant
 
         assertThat(result.getYear(), is(2017));
